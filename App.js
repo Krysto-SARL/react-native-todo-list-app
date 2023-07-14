30f20dd1c30b8703fe1b5db61fcb9f0cf42b090f
Krysto <krysto.contact@gmail.com>
2023-07-14 17:10:07 +1100

    initialize react-native-safe-area

--- App.js
+++ App.js
@@ -1,3 +1,13 @@
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { s } from './App.style'
+import { Text } from 'react-native'
+
 export default function App() {
-  return <></>
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Text style={s.text}>HELLO</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  )
 }
---
+++ App.style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  text: {
+    color: 'blue',
+  },
+})
+
+export { s }
